67f5c274f2ded96f99fa4e3c987ea7d1992a35e2
Sugar <sugarypolecat@gmail.com>
2023-06-01 07:39:44 +0200

    mewo

--- server/server.ts
+++ server/server.ts
@@ -5,27 +5,27 @@ import bodyParser from "body-parser";
 const vuePath: string = path.join(__dirname, "../vue/dist");
 
 async function run(): Promise<void> {
-	const App: Express = express();
-	App.use(cors());
-	App.use(express.static(vuePath));
-	App.use(bodyParser.json());
-	App.use(bodyParser.urlencoded({ extended: true }));
+	const app: Express = express();
+	app.use(cors());
+	app.use(express.static(vuePath));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 
 	try {
-		App.get('*', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+		app.get('*', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
 			response.sendFile(path.join(vuePath, "index.html"));
 		});
 	} catch (err: any) {
-		throw new Error("ERROR  HGAPPENED!!!!" + String(err.message));
+		throw new Error("ERROR  HGappENED!!!!" + String(err.message));
 	}
 
 	const PORT: number = Number(process.env.PORT) || 1501;
 	try {
-		App.listen(PORT, async (): Promise<void> => {
+		app.listen(PORT, async (): Promise<void> => {
 			console.log("listening on port", PORT);
 		});
 	} catch (err: any) {
-		throw new Error("ERROR HAPPENED!!!!" + String(err.message));
+		throw new Error("ERROR HappENED!!!!" + String(err.message));
 	}
 }
 (async (): Promise<void> => {
