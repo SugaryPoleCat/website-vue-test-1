0865b3016586bba0d81f121b34acf0f198b06827
Sugar <sugarypolecat@gmail.com>
2023-06-01 10:00:43 +0200

    asd

--- server/router/routes.ts
+++ server/router/routes.ts
@@ -1,18 +1,20 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
 import { Router } from "express";
-const vuePath: string = path.join(__dirname, "../vue/dist");
+const vuePath: string = path.join(__dirname, "../../vue/dist");
 
-module.exports = async app => {
-	const router: Router = Router();
-	router.use((req: Request, res: Response, next: NextFunction) => {
-		console.log("Time:", new Date().toUTCString());
-		next();
-	});
-	router.get('/', async (req: Request, res: Response) => {
-		// res.send("Hello from index?");
-		res.sendFile(path.join(vuePath, "index.html"));
-		// next();
-	});
-	app.use("/api", router);
-};
+// module.exports = async app => {
+const router: Router = Router();
+router.use((req: Request, res: Response, next: NextFunction) => {
+	console.log("Time:", new Date().toUTCString());
+});
+router.get('/', (req: Request, res: Response) => {
+	res.sendFile(path.join(vuePath, "index.html"));
+});
+router.get("/about", (req, res) => {
+	res.sendFile(path.join(vuePath, "index.html"));
+});
+// app.use("/api", router);
+// };
+
+export default router;
--- server/server.ts
+++ server/server.ts
@@ -2,36 +2,32 @@ import express, { Request, Response, Express, NextFunction } from "express";
 import cors from "cors";
 import path from "path";
 import bodyParser from "body-parser";
+import router from "./router/routes";
 const vuePath: string = path.join(__dirname, "../vue/dist");
-
 async function run(): Promise<void> {
 	const app: Express = express();
-	app.use(cors());
-	app.use(express.static(vuePath));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true }));
-
 	try {
-		app.get('*', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
-			response.sendFile(path.join(vuePath, "index.html"));
-		});
-	} catch (err: any) {
-		throw new Error("ERROR  HGappENED!!!!" + String(err.message));
-	}
-
+		app.use(router);
+		app.use("/api", router);
+		app.use(cors());
+		app.use(express.static(vuePath));
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: true }));
+	} catch (e: any) { throw new Error(String(e.message)); }
+	// try {
+	// 	app.get('*', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+	// 		response.sendFile(path.join(vuePath, "index.html"));
+	// 	});
+	// } catch (err: any) {
+	// 	throw new Error("ERROR  HGappENED!!!!" + String(err.message));
+	// }
 	const PORT: number = Number(process.env.PORT) || 1501;
 	try {
 		app.listen(PORT, async (): Promise<void> => {
 			console.log("listening on port", PORT);
 		});
-	} catch (err: any) {
-		throw new Error("ERROR HappENED!!!!" + String(err.message));
-	}
+	} catch (err: any) { throw new Error("ERROR HappENED!!!!" + String(err.message)); }
 }
 (async (): Promise<void> => {
-	try {
-		await run();
-	} catch (err) {
-		console.error("CONSOLE ERRROR!!!", err);
-	}
+	try { await run(); } catch (err) { console.error("CONSOLE ERRROR!!!", err); }
 })();
